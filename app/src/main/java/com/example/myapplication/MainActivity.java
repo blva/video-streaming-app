1bf575e29fc5f5af467496108949d848ed0e4a06
Bianca L <blvaguiar@gmail.com>
2019-05-27 15:00:13 -0300

    Add video view with MP4 link

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.myapplication;
 
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,10 +17,23 @@ public class MainActivity extends AppCompatActivity {
 
         Button watchButton = findViewById(R.id.button);
 
+
         watchButton.setOnClickListener( view -> {
+            String videoLink = "https://www.radiantmediaplayer.com/media/bbb-360p.mp4";
+            VideoView myView = findViewById(R.id.video);
 
-
-
+            MediaController mediaController = new MediaController(this);
+            mediaController.setAnchorView(myView);
+            myView.setMediaController(mediaController);
+            myView.setVideoURI(Uri.parse(videoLink));
+            myView.setVideoPath(videoLink);
+            myView.requestFocus();
+            myView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                @Override
+                public void onPrepared(MediaPlayer mp) {
+                    myView.start();
+                }
+            });
         });
 
     }
